4428e99063e117b0174958fb190b65d7fb61be0b
xuejieguo <xguo324@gatech.edu>
2020-08-31 22:17:49 -0400

    push into

---
+++ messagingapp/ComposeViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ComposeViewController.swift
+//  messagingapp
+//
+//  Created by Xuejie Guo on 8/30/20.
+//  Copyright © 2020 GT. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class ComposeViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    var ref: DatabaseReference?
+
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ref = Database.database().reference()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func addPost(_ sender: Any) {
+        
+        ref?.child("Posts").childByAutoId().setValue(textView.text)
+        showAddpostAlert()
+    }
+    
+    
+    @IBAction func cancelPost(_ sender: Any) {
+        presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    func showAddpostAlert(){
+        let alert = UIAlertController(title: "Congrats!", message:"You have successfully added this post!" , preferredStyle: .alert)
+        let action = UIAlertAction(title: "Got it", style: .default, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- messagingapp/ViewController.swift
+++ messagingapp/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    @IBOutlet weak var tableView: UITableView!
+    var ref:DatabaseReference?
+    var databaseHandle:DatabaseHandle?
+    
+    var postData = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // set firebase reference
+        ref = Database.database().reference()
+        
+        // retrive the posts and listen for changes
+        databaseHandle = ref?.child("Posts").observe(.childAdded, with: { (snapshot) in
+            
+            //code to execute when a child is added under "posts"
+            //take the value from the snapshot and add to postData array
+            let post = snapshot.value as? String
+            if let actualPost = post{
+                // Append data to postDataArray
+                self.postData.append(actualPost)
+                // reload data in tableview
+                
+                self.tableView.reloadData()
+            }
+            
+            
+        })
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")
+        cell?.textLabel?.text = postData[indexPath.row]
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+      if editingStyle == .delete {
+        self.postData.remove(at: indexPath.row)
+        self.tableView.deleteRows(at: [indexPath], with: .automatic)
+      }
+    }
 
 }
 
